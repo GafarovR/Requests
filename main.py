771ece8df95390651e1f45bafc42fb6a91d3a156
Roman Gafarov <gafarov4@gmail.com>
2022-01-17 21:24:26 +0300

    Added function to upload to Ya.Disk

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from superhero import Super_hero
 from pprint import pprint
+import requests
+
 
 thanos = Super_hero()
 thanos.name = 'Thanos'
@@ -19,4 +21,36 @@ cap.name = 'Captain America'
 # pprint(cap.find_intelligence())
 # pprint(iron_man.find_intelligence())
 
-print(cap.find_the_most_intelligent(hulk, thanos))
+# print(cap.find_the_most_intelligent(hulk, thanos))
+
+
+class YaUploader:
+
+    def __init__(self, token):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+
+    def _get_upload_link(self, disk_file_path):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": disk_file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
+
+    def upload(self, file_path):
+        href = self._get_upload_link(disk_file_path=file_path).get("href", "")
+        response = requests.put(href, data=open(file_path, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Success")
+
+if __name__ == '__main__':
+    file_path = ""
+    uploader = YaUploader(token="")
+    uploader.upload(file_path)
